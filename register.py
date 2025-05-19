a16e8fec6df8e6aa72d5c2d92d33ac37b8191d18
chotika-boon <119413775+chotika-boon@users.noreply.github.com>
2025-05-19 22:42:50 +0700

    Update register.py

--- register.py
+++ register.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from google.oauth2.service_account import Credentials
 import gspread
+import streamlit as st
 
 def insert_to_gsheet(data):
     creds = Credentials.from_service_account_info(
@@ -11,6 +12,7 @@ def insert_to_gsheet(data):
     client = gspread.authorize(creds)
     sheet = client.open_by_key("17os24Dmfczb2qAuy9yUR87vw_6oDWunQulRthw3ZyBg").sheet1
     sheet.append_row(data)
+
 
 def show_register():
     if "register_visited" not in st.session_state:
